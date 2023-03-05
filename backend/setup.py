8cb733d9627a8f6db29a8308b0234db3eaff4d45
axemalt <moonblast3933@gmail.com>
2023-03-05 11:45:17 -0800

    made the table names better

--- backend/setup.py
+++ backend/setup.py
@@ -19,10 +19,19 @@ async def main():
             )
             """)
             await cursor.execute("""
-            CREATE TABLE "user_tasks" (
+            CREATE TABLE "reviewed_tasks" (
                 "task_id" INTEGER,
                 "username" TEXT,
                 "count" INTEGER,
+                FOREIGN KEY("task_id") REFERENCES "task_info"("task_id") ON DELETE CASCADE,
+                FOREIGN KEY("username") REFERENCES "users"("username") ON DELETE CASCADE
+            )
+            """)
+            await cursor.execute("""
+            CREATE TABLE "submitted_tasks" (
+                "task_id" INTEGER,
+                "username" TEXT,
+                "submission" TEXT,
                 FOREIGN KEY("task_id") REFERENCES "task_info"("task_id") ON DELETE CASCADE,
                 FOREIGN KEY("username") REFERENCES "users"("username") ON DELETE CASCADE
             )
