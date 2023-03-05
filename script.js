8dff2d3f7c3614c3a05baaa53fbac598addfa2c9
Yuleen <115421216+YuleenLee@users.noreply.github.com>
2023-03-05 18:11:53 +0000

    edit search

--- script.js
+++ script.js
@@ -4,19 +4,15 @@ let result = document.getElementById('search-results');
 let searchValue = result.getElementsByTagName("h3");
 
 searchForm.addEventListener("keyup", function() {
-  //console.log("Keyup done");
-
   for (var i = 0; i < searchValue.length; i++) {
     console.log(searchForm.value);
-
     let value = searchResultList[i].getElementsByTagName('h3')[0];
-
     let filterValue = value.textContent || value.innerHTML;
-
     if (filterValue.toUpperCase().indexOf(searchForm.value.toUpperCase()) > -1) {
       searchResultList[i].style.display = "";
-    } else {
+    } 
+    else {
       searchResultList[i].style.display = "none";
     }
   }
-})
+});
