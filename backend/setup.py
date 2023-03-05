35fc932caf5e8c776c051cfed0ef5f62084c3a72
axemalt <moonblast3933@gmail.com>
2023-03-05 11:40:17 -0800

    add backend structure and setup database

---
+++ backend/setup.py
@@ -0,0 +1,32 @@
+import asyncio
+import asqlite
+
+async def main():
+    async with asqlite.connect('website.db') as conn:
+        async with conn.cursor() as cursor:
+            await cursor.execute("""
+            CREATE TABLE "users" (
+                "username" TEXT,
+                "password" TEXT,
+                PRIMARY KEY("username")
+            )
+            """)
+            await cursor.execute("""
+            CREATE TABLE "task_info" (
+                "task_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "limit" INTEGER,
+                "points" INTEGER
+            )
+            """)
+            await cursor.execute("""
+            CREATE TABLE "user_tasks" (
+                "task_id" INTEGER,
+                "username" TEXT,
+                "count" INTEGER,
+                FOREIGN KEY("task_id") REFERENCES "task_info"("task_id") ON DELETE CASCADE,
+                FOREIGN KEY("username") REFERENCES "users"("username") ON DELETE CASCADE
+            )
+            """)
+            await conn.commit()
+
+asyncio.run(main())
