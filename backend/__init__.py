c4e02579ff40926dfa752a8b6cd90511173969db
axemalt <moonblast3933@gmail.com>
2023-03-05 13:51:14 -0800

    add creating account

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,8 @@
-from quart import Quart
+from quart import Quart, request, abort
 import aiohttp
 import asyncio
 import asqlite
+import json
 
 app = Quart(__name__)
 session = None
@@ -10,6 +11,36 @@ conn = None
 @app.route("/")
 async def main():
     return "Online"
+
+@app.route("/create_account", methods=["POST"])
+async def create_account():
+    data = await request.get_json(force=True)
+
+    try:
+        username = data["username"]
+        password = data["password"]
+    except KeyError:
+        abort(400)
+
+    async with conn.cursor() as cursor:
+        user = await (await cursor.execute("""
+        SELECT 1
+        FROM user_info
+        WHERE username = ?
+        """,
+        (username))).fetchone()
+
+        if user is not None:
+            abort(400)
+
+        await cursor.execute("""
+        INSERT INTO user_info
+        VALUES (?, ?, ?)
+        """,
+        (username, password, 0))
+        await conn.commit()
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 async def run():
     global session, cursor
