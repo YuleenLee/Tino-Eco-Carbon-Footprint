a332f74f5ff1f1f008a97caf4753ccfe3a606e00
axemalt <moonblast3933@gmail.com>
2023-03-06 22:01:20 -0800

    add is_officer column to user_info table

--- backend/setup.py
+++ backend/setup.py
@@ -9,6 +9,7 @@ async def main():
                 "username" TEXT,
                 "password" TEXT,
                 "points" INTEGER DEFAULT 0 CHECK(points>=0),
+                "is_officer" BOOLEAN NOT NULL,
                 PRIMARY KEY("username")
             )
             """)
