e489f1191b0e097915bcd4547602a509401ffb1e
axemalt <moonblast3933@gmail.com>
2023-03-09 22:36:19 -0800

    add account creating

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,26 @@
+function create_account(username, password) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+        "POST",
+        "http://localhost:3768/create_account"
+    );
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    const body = JSON.stringify({
+        "username": username,
+        "password": password,
+    });
+    xhr.onload = () => {
+        if (xhr.readyState == 4) {
+            const data = JSON.parse(xhr.responseText);
+            if (xhr.status == 404) {
+                alert(data["message"]);
+            }
+        }
+    }
+    xhr.send(body);
+    alert("Account created.");
+}
+
 function login(username, password) {
     const xhr = new XMLHttpRequest();
     xhr.open(
