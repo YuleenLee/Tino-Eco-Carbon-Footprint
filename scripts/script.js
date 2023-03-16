b76060068e2f4aa3d5faea4b42de205c091ddecf
axemalt <moonblast3933@gmail.com>
2023-03-15 17:35:21 -0700

    fix search and load the right file lol

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,26 @@
-let searchForm = document.getElementById('search-form');
-let searchResultList = document.querySelectorAll('.result-items');
-let result = document.getElementById('search-results');
-let searchValue = result.getElementsByTagName('h3');
+window.onload = function() {
+  let searchForm = document.getElementById('search-form');
+  console.log(searchForm);
+  let searchResultList = document.querySelectorAll('.result-items');
+  let result = document.getElementById('search-results');
+  console.log(searchResultList);
+  console.log(result);
+  let searchValue = result.getElementsByTagName('h3');
 
-searchForm.addEventListener("keyup", function() {
-  for (var i = 0; i < searchValue.length; i++) {
-    console.log(searchForm.value);
-    let value = searchResultList[i].getElementsByTagName('h3')[0];
-    let filterValue = value.textContent || value.innerHTML;
-    if (filterValue.toUpperCase().indexOf(searchForm.value.toUpperCase()) > -1) {
-      searchResultList[i].style.display = "";
-    } 
-    else {
-      searchResultList[i].style.display = "none";
+  searchForm.addEventListener("keyup", function() {
+    for (var i = 0; i < searchValue.length; i++) {
+      console.log(searchForm.value);
+      let value = searchResultList[i].getElementsByTagName('h3')[0];
+      let filterValue = value.textContent || value.innerHTML;
+      if (filterValue.toUpperCase().indexOf(searchForm.value.toUpperCase()) > -1) {
+        searchResultList[i].style.display = "";
+      } 
+      else {
+        searchResultList[i].style.display = "none";
+      }
     }
-  }
-});
+  });
+}
 
 function displayDetails() {
   var description = document.getElementById("taskinfo");
