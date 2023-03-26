dab473a5592d7f667d2b6847867d33ccfe090c17
axemalt <moonblast3933@gmail.com>
2023-03-26 14:44:29 -0700

    change to sync

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,36 +1,37 @@
-from quart import Quart, Response, request, abort
+from flask import Flask, Response, request, abort
 from datetime import datetime, timedelta
 import hashlib
-import asyncio
-import asqlite
+import sqlite3
 
 HEADERS = {
     'Access-Control-Allow-Origin': '*',
 }
 
-app = Quart(__name__)
-conn = None
+app = Flask(__name__)
 
-async def is_officer(email):
-    async with conn.cursor() as cursor:
-        user = await (await cursor.execute("""SELECT 1 FROM user_info WHERE is_officer = 1 AND email = ?""", (email))).fetchone()
+def is_officer(email):
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        user = cursor.execute("""SELECT 1 FROM user_info WHERE is_officer = 1 AND email = ?""", [email])
+        user = user.fetchone()
         return user is not None
 
 @app.route("/")
-async def main():
+def main():
     return "Online."
 
 @app.post("/user_points")
-async def user_points():
-    data = await request.get_json(force=True)
+def user_points():
+    data = request.get_json(force=True)
 
     try:
         email = data["email"]
     except KeyError:
         abort(400)
         
-    async with conn.cursor() as cursor:
-        points_row = await (await cursor.execute("SELECT points FROM user_info WHERE email = ?", (email))).fetchone()
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        points_row = cursor.execute("SELECT points FROM user_info WHERE email = ?", [email]).fetchone()
 
     if points_row is None:
         return {'message': 'Invalid user.'}, 404, HEADERS
@@ -38,8 +39,8 @@ async def user_points():
     return {"points": points_row[0]}, 200, HEADERS
 
 @app.post("/is_valid_session")
-async def is_valid_session():
-    data = await request.get_json(force=True)
+def is_valid_session():
+    data = request.get_json(force=True)
     
     try:
         email = data["email"]
@@ -49,32 +50,33 @@ async def is_valid_session():
     except TypeError:
         {"is_valid_session": False}, 200, HEADERS
 
-    async with conn.cursor() as cursor:
-        session = await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        session = cursor.execute("""
         SELECT expires
         FROM session_info
         WHERE session_id = ? AND email = ?
         """,
-        (session_id, email))).fetchone()
+        [session_id, email]).fetchone()
 
     if session is None or session[0] < datetime.now():
         return {"is_valid_session": False}, 200, HEADERS
     return {"is_valid_session": True}, 200, HEADERS
 
 @app.post("/is_officer")
-async def officer():
-    data = await request.get_json(force=True)
+def officer():
+    data = request.get_json(force=True)
 
     try:
         email = data["email"]
     except KeyError:
         abort(400)
         
-    return {"is_officer": await is_officer(email)}, 200, HEADERS
+    return {"is_officer": is_officer(email)}, 200, HEADERS
 
 @app.post("/create_account")
-async def create_account():
-    data = await request.get_json(force=True)
+def create_account():
+    data = request.get_json(force=True)
 
     try:
         name = data["name"]
@@ -83,29 +85,30 @@ async def create_account():
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        user = await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        user = cursor.execute("""
         SELECT 1
         FROM user_info
         WHERE email = ?
         """,
-        (email))).fetchone()
+        [email]).fetchone()
 
         if user is not None:
             return {'message': 'A user with that email already exists.'}, 404, HEADERS
 
-        await cursor.execute("""
+        cursor.execute("""
         INSERT INTO user_info(email, name, password)
         VALUES (?, ?, ?)
         """,
-        (email, name, password))
-        await conn.commit()
+        [email, name, password])
+        conn.commit()
 
     return Response(status=201, headers=HEADERS)
 
 @app.post("/login")
-async def login():
-    data = await request.get_json(force=True)
+def login():
+    data = request.get_json(force=True)
     
     try:
         email = data["email"]
@@ -113,41 +116,43 @@ async def login():
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        user = await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        user = cursor.execute("""
         SELECT 1
         FROM user_info
         WHERE email = ? AND password = ?
         """,
-        (email, password))).fetchone()
+        [email, password]).fetchone()
 
         if user is None:
             return {'message': 'Invalid login information.'}, 404, HEADERS
         
         now = datetime.now()
         session_id = hashlib.sha256(f"{email}{password}{now}".encode()).hexdigest()
-        await cursor.execute("""
+        cursor.execute("""
         INSERT INTO session_info
         VALUES (?, ?, ?)
         """,
-        (session_id, email, now + timedelta(days=7)))
+        [session_id, email, now + timedelta(days=7)])
 
     return {"session_id": session_id}, 201, HEADERS
 
 @app.post("/submitted_tasks")
-async def submitted_tasks():
-    data = await request.get_json(force=True)
+def submitted_tasks():
+    data = request.get_json(force=True)
 
     try:
         email = data["email"]
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        if await is_officer(email):
-            stuff = await (await cursor.execute("SELECT * FROM tasks WHERE accepted = 0")).fetchall()
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        if is_officer(email):
+            stuff = cursor.execute("SELECT * FROM tasks WHERE accepted = 0").fetchall()
         else:
-            stuff = await (await cursor.execute("SELECT * FROM tasks WHERE accepted = 0 AND email = ?", (email))).fetchall()
+            stuff = cursor.execute("SELECT * FROM tasks WHERE accepted = 0 AND email = ?", [email]).fetchall()
         
     tasks = []
     for row in stuff:
@@ -161,19 +166,20 @@ async def submitted_tasks():
     return {'data': tasks}, 200, HEADERS
 
 @app.post("/accepted_tasks")
-async def accepted_tasks():
-    data = await request.get_json(force=True)
+def accepted_tasks():
+    data = request.get_json(force=True)
 
     try:
         email = data["email"]
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        if await is_officer(email):
-            stuff = await (await cursor.execute("SELECT * FROM tasks WHERE accepted = 1")).fetchall()
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        if is_officer(email):
+            stuff = cursor.execute("SELECT * FROM tasks WHERE accepted = 1").fetchall()
         else:
-            stuff = await (await cursor.execute("SELECT * FROM tasks WHERE accepted = 1 AND email = ?", (email))).fetchall()
+            stuff = cursor.execute("SELECT * FROM tasks WHERE accepted = 1 AND email = ?", [email]).fetchall()
         
     tasks = []
     for row in stuff:
@@ -187,14 +193,15 @@ async def accepted_tasks():
     return {'data': tasks}, 200, HEADERS
 
 @app.get("/leaderboard")
-async def leaderboard():
+def leaderboard():
     users = []
-    async with conn.cursor() as cursor:
-        for row in await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        for row in cursor.execute("""
         SELECT name, points FROM user_info
         WHERE points > 0
         ORDER BY points DESC
-        """)).fetchall():
+        """).fetchall():
             users.append({
                 "name": row[0],
                 "points": row[1],
@@ -203,8 +210,8 @@ async def leaderboard():
     return {'data': users}, 200, HEADERS
 
 @app.post("/submit_task")
-async def submit_task():
-    data = await request.get_json(force=True)
+def submit_task():
+    data = request.get_json(force=True)
 
     try:
         email = data["email"]
@@ -213,20 +220,21 @@ async def submit_task():
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        count = (await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        count = cursor.execute("""
         SELECT COUNT(*)
         FROM tasks
         WHERE task_id = ? AND email = ?
         """,
-        (task_id, email))).fetchone())[0]
+        [task_id, email]).fetchone()[0]
         
-        task_limit = await (await cursor.execute("""
+        task_limit = cursor.execute("""
         SELECT "limit"
         FROM task_info
         WHERE task_id = ?
         """,
-        (task_id))).fetchone()
+        [task_id]).fetchone()
 
         if task_limit is None:
             return {'message': 'Invalid task.'}, 404, HEADERS
@@ -234,18 +242,18 @@ async def submit_task():
         if task_limit[0] != -1 and count >= task_limit[0]:
             return {'message': 'Maximum submissions reached.'}, 404, HEADERS
 
-        await cursor.execute("""
+        cursor.execute("""
         INSERT INTO tasks(task_id, email, submission, accepted)
         VALUES (?, ?, ?, ?)
         """,
-        (task_id, email, submission, 0))
-        await conn.commit()
+        [task_id, email, submission, 0])
+        conn.commit()
 
     return Response(status=201, headers=HEADERS)
 
 @app.post("/review_task")
-async def review_task():
-    data = await request.get_json(force=True)
+def review_task():
+    data = request.get_json(force=True)
 
     try:
         submission_id = int(data["submission_id"])
@@ -254,12 +262,13 @@ async def review_task():
     except KeyError:
         abort(400)
 
-    async with conn.cursor() as cursor:
-        task = await (await cursor.execute("""
+    with sqlite3.connect('website.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        cursor = conn.cursor()
+        task = cursor.execute("""
         SELECT email, accepted FROM tasks
         WHERE submission_id = ?
         """,
-        (submission_id))).fetchone()
+        [submission_id]).fetchone()
 
         if task is None:
             return {'message': 'Task does not exist.'}, 404, HEADERS
@@ -268,39 +277,33 @@ async def review_task():
             return {'message': 'Task already accepted.'}, 404, HEADERS
         
         if accepted:
-            await cursor.execute("""
+            cursor.execute("""
             UPDATE tasks
             SET accepted = 1
             WHERE submission_id = ?
             """,
-            (submission_id))
+            [submission_id])
 
-            user_points = (await (await cursor.execute("""
+            user_points = cursor.execute("""
             SELECT points FROM user_info
             WHERE email = ?
             """,
-            (task[0]))).fetchone())[0]
-            await cursor.execute("""
+            [task[0]]).fetchone()[0]
+            cursor.execute("""
             UPDATE user_info
             SET points = ?
             WHERE email = ?
             """,
-            (user_points + points, task[0]))
+            [user_points + points, task[0]])
         else:
-            await cursor.execute("""
+            cursor.execute("""
             DELETE FROM tasks
             WHERE submission_id = ?
             """,
-            (submission_id))
+            [submission_id])
         
-        await conn.commit()
+        conn.commit()
 
     return Response(status=201, headers=HEADERS)
 
-async def run():
-    global conn
-    async with asqlite.connect('website.db', detect_types=asqlite.PARSE_DECLTYPES | asqlite.PARSE_COLNAMES) as c:
-        conn = c
-        await app.run_task(port="8000")
-
-asyncio.run(run())
+app.run(port="8000")
