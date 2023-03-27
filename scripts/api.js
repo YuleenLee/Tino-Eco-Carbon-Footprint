d9f9c9e2ac0a4127192efb37b0c89a5f3f3447c0
axemalt <moonblast3933@gmail.com>
2023-03-26 22:02:14 -0700

    forgot to uncomment

--- scripts/api.js
+++ scripts/api.js
@@ -155,9 +155,9 @@ function fill_leaderboard() {
         "GET",
         `${apiURL}leaderboard`,
     );
-    // xhr.onerror = function(e){
-    //     window.location.href = "oops.html";
-    // };
+    xhr.onerror = function(e){
+        window.location.href = "oops.html";
+    };
     xhr.onload = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             const data = JSON.parse(xhr.responseText)["data"];
