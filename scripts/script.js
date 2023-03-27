0d7205dc23edb641192bf96291c3d6e219a8a630
Yuleen <115421216+YuleenLee@users.noreply.github.com>
2023-03-27 05:15:58 +0000

    search thing

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@ window.onload = function() {
   let searchForm = document.getElementById('search-form');
   let searchResultList = document.querySelectorAll('.result-items');
   let result = document.getElementById('search-results');
-  let searchValue = result.getElementsByTagName('h3');
+  let searchValue = result.getElementsByTagName('p');
 
   searchForm.addEventListener("keyup", function() {
     for (var i = 0; i < searchValue.length; i++) {
       console.log(searchForm.value);
-      let value = searchResultList[i].getElementsByTagName('h3')[0];
+      let value = searchResultList[i].getElementsByTagName('p')[0];
       let filterValue = value.textContent || value.innerHTML;
       if (filterValue.toUpperCase().indexOf(searchForm.value.toUpperCase()) > -1) {
         searchResultList[i].style.display = "";
